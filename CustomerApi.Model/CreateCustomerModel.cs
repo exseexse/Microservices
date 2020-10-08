ae871be7786b152c258ac4d0e19e6f1e261534e4
asdasd asdasdas <sukrukohle@hotmail.com>
2020-10-08 20:55:05 +0300

    New Model Added

---
+++ CustomerApi.Model/CreateCustomerModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CustomerApi.Model
+{
+    public class CreateCustomerModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public DateTime? Birthday { get; set; }
+
+        public int? Age { get; set; }
+    }
+}
